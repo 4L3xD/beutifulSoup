9e5e0a692914251be534b07325bd3408d252d326
4L3_xD <alessandra.olea@gmail.com>
2020-03-10 20:58:18 -0300

    writing the Data to a CSV File

--- nga_z_artists.py
+++ nga_z_artists.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 # Collect first page of artists’ list
@@ -11,6 +12,10 @@ soup = BeautifulSoup(page.text, 'html.parser')
 last_links = soup.find(class_='AlphaNav')
 last_links.decompose()
 
+# Create a file to write to, add headers row
+f = csv.writer(open('z-artist-names.csv', 'w'))
+f.writerow(['Name', 'Link'])
+
 # Pull all text from the BodyText div
 artist_name_list = soup.find(class_='BodyText')
 
@@ -21,5 +26,9 @@ artist_name_list_items = artist_name_list.find_all('a')
 # Create for loop to print out all artists' names
 for artist_name in artist_name_list_items:
     names = artist_name.contents[0]
+    links = 'https://web.archive.org' + artist_name.get('href')
     print(names)
+    print(links)
 
+    # Add each artist’s name and associated link to a row
+    f.writerow([names, links])
