49974c579f85d421b054ba549c9700a4b92e75d6
4L3_xD <alessandra.olea@gmail.com>
2020-03-10 21:12:07 -0300

    finish

--- nga_z_artists.py
+++ nga_z_artists.py
@@ -2,33 +2,29 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-# Collect first page of artists’ list
-page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+pages = []
 
-# Create a BeautifulSoup object
-soup = BeautifulSoup(page.text, 'html.parser')
-
-# Remove bottom links
-last_links = soup.find(class_='AlphaNav')
-last_links.decompose()
-
-# Create a file to write to, add headers row
 f = csv.writer(open('z-artist-names.csv', 'w'))
 f.writerow(['Name', 'Link'])
 
-# Pull all text from the BodyText div
-artist_name_list = soup.find(class_='BodyText')
+for i in range(1, 5):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
+    pages.append(url)
 
-# Pull text from all instances of <a> tag within BodyText div
-artist_name_list_items = artist_name_list.find_all('a')
+for item in pages:
+    page = requests.get(item)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
+    last_links = soup.find(class_='AlphaNav')
+    last_links.decompose()
 
-# Create for loop to print out all artists' names
-for artist_name in artist_name_list_items:
-    names = artist_name.contents[0]
-    links = 'https://web.archive.org' + artist_name.get('href')
-    print(names)
-    print(links)
+    artist_name_list = soup.find(class_='BodyText')
+    artist_name_list_items = artist_name_list.find_all('a')
 
-    # Add each artist’s name and associated link to a row
-    f.writerow([names, links])
+    for artist_name in artist_name_list_items:
+        names = artist_name.contents[0]
+        links = 'https://web.archive.org' + artist_name.get('href')
+
+        f.writerow([names, links])
+
+# https://www.digitalocean.com/community/tutorials/how-to-scrape-web-pages-with-beautiful-soup-and-python-3
